105a19f1dc96ac19c2431a86448afb42846be251
haisyamm <haisyammaulana22@gmail.com>
2023-02-10 10:13:00 +0700

    penambahan link agar dapat menggunakan filter date range

--- resources/views/report/petugas.blade.php
+++ resources/views/report/petugas.blade.php
@@ -11,26 +11,26 @@
                     <div class="col-lg-3">
                         <div class="form-group">
                             <label for="smallSelect">TANGGAL AWAL</label>
-                            <input type="date" class="form-control" name="tgL_payment" value="{{ request()->tgL_payment ? request()->tgL_payment : "" }}">
+                            <input type="date" class="form-control" name="from_date" value="{{ request()->from_date ? request()->from_date : "" }}">
                         </div>
                     </div>
                     <div class="col-lg-3">
                         <div class="form-group">
                         <label for="smallSelect">TANGGAL AKHIR</label>
-                            <input type="date" class="form-control" name="created_date" value="{{ request()->created_date ? request()->created_date : "" }}">
+                            <input type="date" class="form-control" name="to_date" value="{{ request()->to_date ? request()->to_date : "" }}">
                         </div>
                     </div>
                     <div class="col-lg-2">
                         <button style="margin-top: 20px" class="btn btn-primary"><i class="flaticon-interface-4"></i> FILTER</button>
                     </div>
                     <div class="col-lg-2">
-                    <a target="_blank" href="{{route('report.petugas.pdf')}}" class="btn btn-secondary btn-round ml-auto">
+                    <a target="_blank" href="{{route('report.petugas.pdf', ['from_date' => request()->from_date, 'to_date' => request()->to_date])}}" class="btn btn-secondary btn-round ml-auto">
                         <i class="fa fa-file-pdf"></i>
                         DOWNLOAD PDF
                     </a>
                     </div>
                     <div class="col-lg-2">
-                    <a target="_blank" href="" class="btn btn-default btn-round ">
+                    <a target="_blank" href="{{route('report.petugas.excel', ['from_date' => request()->from_date, 'to_date' => request()->to_date])}}" class="btn btn-default btn-round ">
                         <i class="fas fa-file-download"></i>
                         DOWNLOAD DATA
                     </a>
